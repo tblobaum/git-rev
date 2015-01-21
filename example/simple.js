a1aafc651c0c180790c9480147aa2013405a4389
kurttheviking <kurttheviking@outlook.com>
2015-01-21 13:20:47 -0800

    update 1.x examples

--- example/simple.js
+++ example/simple.js
@@ -2,31 +2,15 @@
 
 var git = require('../')
 
-console.log('[git.short()]', git.short());
+console.log('git.short() => ' + git.short());
 // 75bf4ee
 
-console.log('[git.long()]', git.long());
+console.log('git.long() => ' + git.long());
 // 75bf4eea9aa1a7fd6505d0d0aa43105feafa92ef
 
-console.log('[git.branch()]', git.branch());
+console.log('git.branch() => ' + git.branch());
 // master
 
-console.log('[git.tag()]', git.tag());
-// 0.1.0
+// console.log('git.tag() => ', git.tag());
 
-console.log('[git.log()]', git.log());
-// [
-//   [
-//     '75bf4eea9aa1a7fd6505d0d0aa43105feafa92ef',
-//     'update pjson to include sync exec',
-//     '17 minutes ago',
-//     'kurttheviking'
-//   ],
-//   [
-//     '143120ac3ecc07aeae1462b372bb2033aa20c3ee',
-//     'Merge pull request #6 from shtylman/patch-1',
-//     '1 year, 2 months ago',
-//     'Thomas Blobaum'
-//   ],
-//   ...
-// ]
+// console.log('git.log() => ', git.log());
