85c3ff5aab4e4d64b850339377f718e98df26e93
Roman Jurkov <winfinit@gmail.com>
2015-07-10 14:07:40 -0400

    moved subject line to the end, added separators between fields, preserved previous order after formatter was changed

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 var exec = require('child_process').exec
 
 function _command (cmd, cb) {
-  exec(cmd, { cwd: __dirname }, function (err, stdout, stderr) {
-    cb(stdout.split('\n').join(''))
+  exec(cmd, function (err, stdout, stderr) {
+    cb(stdout.slice(0, -1));
   })
 }
 
-module.exports = { 
-    short : function (cb) { 
+module.exports = {
+    short : function (cb) {
       _command('git rev-parse --short HEAD', cb)
     }
-  , long : function (cb) { 
+  , long : function (cb) {
       _command('git rev-parse HEAD', cb)
     }
-  , branch : function (cb) { 
+  , branch : function (cb) {
       _command('git rev-parse --abbrev-ref HEAD', cb)
     }
-  , tag : function (cb) { 
+  , tag : function (cb) {
       _command('git describe --always --tag --abbrev=0', cb)
     }
-  , log : function (cb) { 
-      _command('git log --no-color --pretty=format:\'[ "%H", "%s", "%cr", "%an" ],\' --abbrev-commit', function (str) {
-        str = str.substr(0, str.length-1)
-        cb(JSON.parse('[' + str + ']'))
+  , log : function (cb) {
+      _command('git log --no-color --pretty=format:\'%H  |  %cr  |  %an  |  %s\' --abbrev-commit', function (str) {
+        var logs = [];
+        (str.split('\n')).forEach(function(line, line_idx) {
+            var parsed_line = line.split('  |  ');
+            logs.push([
+                parsed_line[0],
+                parsed_line[3],
+                parsed_line[1],
+                parsed_line[2]
+            ]);
+        });
+        cb(logs)
       })
     }
 }
