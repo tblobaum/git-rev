583214d2f6c84270a31d20c5280ee3728467c80e
Giacomo Manna <g.manna@beeweeb.com>
2016-07-13 10:29:32 +0200

    implemented exactTag command

--- index.js
+++ index.js
@@ -6,23 +6,33 @@ function _command (cmd, cb) {
   })
 }
 
-module.exports = { 
-    short : function (cb) { 
+module.exports = {
+    short : function (cb) {
       _command('git rev-parse --short HEAD', cb)
     }
-  , long : function (cb) { 
+  , long : function (cb) {
       _command('git rev-parse HEAD', cb)
     }
-  , branch : function (cb) { 
+  , branch : function (cb) {
       _command('git rev-parse --abbrev-ref HEAD', cb)
     }
-  , tag : function (cb) { 
+  , tag : function (cb) {
       _command('git describe --always --tag --abbrev=0', cb)
     }
-  , log : function (cb) { 
+  , log : function (cb) {
       _command('git log --no-color --pretty=format:\'[ "%H", "%s", "%cr", "%an" ],\' --abbrev-commit', function (str) {
         str = str.substr(0, str.length-1)
         cb(JSON.parse('[' + str + ']'))
       })
     }
+  , exactTag: function(cb){
+    _command('git describe --exact-match --tags HEAD',function(str){
+      if (str){
+        cb(str)
+      }else{
+        cb(undefined)
+      }
+
+    })
+  }
 }
